2593a7077a7f37d48b57ea25a1cec3805bcaac1c
Deray <33969267+LOoLzeC@users.noreply.github.com>
2018-09-12 10:55:02 +0700

    Add files via upload

---
+++ core/tools/options/interpreter_options.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# (C) Copyright 2018 LOoLzeC Scurity
+# Date : 14 maret 2018
+# Hasches framework By Deray
+# Contact instagram @reyy05_
+from colors import *
+def iop():
+	print 
+	print "%sOPTIONS\n%s======\n\n"%(WHITEBOLD,RED)
+	print "%s\tName\t\tDescription%s"%(WHITEBOLD,RED)
+	print "\t----\t\t-----------"
+	print "\tshow modules\tmodules collections"
+	print "\tclear\t\tclear screen"
+	print "\texit\t\tBye."
+	print "\thash\t\te.g hash <STRING>"
+	print 
