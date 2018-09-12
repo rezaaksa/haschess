d6332980c8b23393665e610eb8808c84af5f4542
Deray <33969267+LOoLzeC@users.noreply.github.com>
2018-09-12 10:12:18 +0700

    Add files via upload

---
+++ hasches.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# (C) Copyright 2018 LOoLzeC Scurity
+# Date : 14 maret 2018
+# Hasches framework By Deray
+# Contact instagram @reyy05_
+#
+from core.tools.banner import *
+print banner
+from core import interpreter
+interpreter.main()
