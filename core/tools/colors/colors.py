470584176232a6c3c31caf09cfc5e5b661568fe9
Deray <33969267+LOoLzeC@users.noreply.github.com>
2018-09-12 10:53:32 +0700

    Add files via upload

---
+++ core/tools/colors/colors.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# (C) Copyright 2018 LOoLzeC Scurity
+# Date : 14 maret 2018
+# Hasches framework By Deray
+# Contact instagram @reyy05_
+WHITEBOLD = '\033[1;37m' # White bold
+WHITE  = '\033[0m'  # white (default)
+RED = '\033[31m' # red
+GREEN  = '\033[1;32m' # green bold
+ORANGE  = '\033[33m' # orange
+BLUE  = '\033[34m' # blue
+PURPLE  = '\033[35m' # purple
+CYAN  = '\033[36m' # cyan
+GRAY = '\033[37m' # gray
+LINE = '\001\033[4m\002'
+STOPLINE =  '\001\033[0m\002'
